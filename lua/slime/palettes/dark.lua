8ae4fa4caf98b26e04b03949bb2fe5c87d26a231
Geoff Gard <gardgeoff@gmail.com>
2026-05-04 17:49:49 -0700

    fix colorscheme for properties

--- lua/slime/palettes/dark.lua
+++ lua/slime/palettes/dark.lua
@@ -3,43 +3,43 @@ local utils = require("slime.utils")
 local M = {}
 
 local base = {
-    bg = "#1E2324",
-    fg = "#D3D7CF",
-    selection = "#2A2F30",
-    line = "#252B2C",
-    cursor = "#F5F5F5",
-    comment = "#6E7573"
+	bg = "#1E2324",
+	fg = "#D3D7CF",
+	selection = "#2A2F30",
+	line = "#252B2C",
+	cursor = "#F5F5F5",
+	comment = "#6E7573",
 }
 
 local semantic = {
-    keyword = "#D8D29A", -- Control flow keywords
-    declaration = "#D88F56", -- Function/class declarations
-    func = "#E0BA7D",
-    method = "#E0BA7D",
-    property = "#BE9296",
-    type = "#E6CE64",
-    string = "#8CAEC1",
-    number = "#B081B9",
-    operator = "#CCD2BE",
+	keyword = "#D8D29A",
+	declaration = "#D88F56",
+	func = "#E0BA7D",
+	method = "#E0BA7D",
+	property = "#BE9296",
+	type = "#E6CE64",
+	string = "#8CAEC1",
+	number = "#B081B9",
+	operator = "#CCD2BE",
 
-    error = "#B64E4E",
-    warning = "#91B147",
-    info = "#6A9FB5",
-    hint = "#7B9C77",
+	error = "#B64E4E",
+	warning = "#91B147",
+	info = "#6A9FB5",
+	hint = "#7B9C77",
 
-    git_add = "#91B147",
-    git_change = "#D8D29A",
-    git_delete = "#B64E4E",
+	git_add = "#91B147",
+	git_change = "#D8D29A",
+	git_delete = "#B64E4E",
 
-    accent1 = "#D8D29A",
-    accent2 = "#D88F56",
-    accent3 = "#6A9FB5"
+	accent1 = "#D8D29A",
+	accent2 = "#D88F56",
+	accent3 = "#6A9FB5",
 }
 
 local plugin = {
-    telescope_bg = "#232A2B",
-    telescope_prompt_bg = "#1A1F20",
-    telescope_selection_bg = "#2D3435"
+	telescope_bg = "#232A2B",
+	telescope_prompt_bg = "#1A1F20",
+	telescope_selection_bg = "#2D3435",
 }
 
 M.colors = vim.tbl_extend("force", base, semantic, plugin)
@@ -50,22 +50,22 @@ M.colors.search = utils.blend(semantic.accent1, base.bg, 0.3)
 
 -- Terminal colors
 M.terminal_colors = {
-    terminal_color_0 = base.bg,
-    terminal_color_1 = semantic.error,
-    terminal_color_2 = semantic.git_add,
-    terminal_color_3 = semantic.warning,
-    terminal_color_4 = semantic.info,
-    terminal_color_5 = semantic.number,
-    terminal_color_6 = semantic.string,
-    terminal_color_7 = base.fg,
-    terminal_color_8 = base.comment,
-    terminal_color_9 = utils.lighten(semantic.error, 0.2),
-    terminal_color_10 = utils.lighten(semantic.git_add, 0.2),
-    terminal_color_11 = utils.lighten(semantic.warning, 0.2),
-    terminal_color_12 = utils.lighten(semantic.info, 0.2),
-    terminal_color_13 = utils.lighten(semantic.number, 0.2),
-    terminal_color_14 = utils.lighten(semantic.string, 0.2),
-    terminal_color_15 = utils.lighten(base.fg, 0.2)
+	terminal_color_0 = base.bg,
+	terminal_color_1 = semantic.error,
+	terminal_color_2 = semantic.git_add,
+	terminal_color_3 = semantic.warning,
+	terminal_color_4 = semantic.info,
+	terminal_color_5 = semantic.number,
+	terminal_color_6 = semantic.string,
+	terminal_color_7 = base.fg,
+	terminal_color_8 = base.comment,
+	terminal_color_9 = utils.lighten(semantic.error, 0.2),
+	terminal_color_10 = utils.lighten(semantic.git_add, 0.2),
+	terminal_color_11 = utils.lighten(semantic.warning, 0.2),
+	terminal_color_12 = utils.lighten(semantic.info, 0.2),
+	terminal_color_13 = utils.lighten(semantic.number, 0.2),
+	terminal_color_14 = utils.lighten(semantic.string, 0.2),
+	terminal_color_15 = utils.lighten(base.fg, 0.2),
 }
 
 return M
