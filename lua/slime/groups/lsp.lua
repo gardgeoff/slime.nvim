19b9ed43b6881eed8d593be4b864e7edaa5c140e
Geoff Gard <gardgeoff@gmail.com>
2026-05-14 17:04:15 -0700

    revert lsp changes

--- lua/slime/groups/lsp.lua
+++ lua/slime/groups/lsp.lua
@@ -1,71 +1,111 @@
 local utils = require("slime.utils")
+
 local M = {}
 
 function M.setup(colors, config)
-	utils.highlight("@keyword", { fg = colors.keyword }, config)
-	utils.highlight("@keyword.function", { fg = colors.declaration }, config)
-	utils.highlight("@keyword.operator", { fg = colors.operator }, config)
-	utils.highlight("@keyword.return", { fg = colors.keyword }, config)
+    -- LSP semantic tokens
+    -- utils.highlight("@lsp.type.class", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.decorator", {
+    --     fg = colors.declaration
+    -- }, config)
+    -- utils.highlight("@lsp.type.enum", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.enumMember", {
+    --     fg = colors.property
+    -- }, config)
+    -- utils.highlight("@lsp.type.function", {
+    --     fg = colors.func
+    -- }, config)
+    -- utils.highlight("@lsp.type.interface", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.macro", {
+    --     fg = colors.declaration
+    -- }, config)
+    -- utils.highlight("@lsp.type.method", {
+    --     fg = colors.method
+    -- }, config)
+    -- utils.highlight("@lsp.type.namespace", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.parameter", {
+    --     fg = colors.fg,
+    --     italic = true
+    -- }, config)
+    -- utils.highlight("@lsp.type.property", {
+    --     fg = colors.property
+    -- }, config)
+    -- utils.highlight("@lsp.type.struct", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.type", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.typeParameter", {
+    --     fg = colors.type
+    -- }, config)
+    -- utils.highlight("@lsp.type.variable", {
+    --     fg = colors.hint
+    -- }, config)
 
-	utils.highlight("@function", { fg = colors.func }, config)
-	utils.highlight("@function.builtin", { fg = colors.keyword }, config)
-	utils.highlight("@function.method", { fg = colors.method }, config)
-	utils.highlight("@function.method.call", { fg = colors.method }, config)
+    -- -- LSP modifiers
+    -- utils.highlight("@lsp.mod.declaration", {
+    --     fg = colors.declaration
+    -- }, config)
+    -- utils.highlight("@lsp.mod.definition", {
+    --     fg = colors.fg,
+    --     bold = true
+    -- }, config)
+    -- utils.highlight("@lsp.mod.deprecated", {
+    --     fg = colors.comment,
+    --     strikethrough = true
+    -- }, config)
+    -- utils.highlight("@lsp.mod.documentation", {
+    --     fg = colors.comment
+    -- }, config)
+    -- utils.highlight("@lsp.mod.readonly", {
+    --     fg = colors.fg,
+    --     italic = true
+    -- }, config)
+    -- utils.highlight("@lsp.mod.static", {
+    --     fg = colors.declaration
+    -- }, config)
 
-	utils.highlight("@constructor", { fg = colors.type }, config)
+    -- -- LSP document highlighting
+    -- utils.highlight("LspReferenceText", {
+    --     bg = colors.selection
+    -- }, config)
+    -- utils.highlight("LspReferenceRead", {
+    --     bg = colors.selection
+    -- }, config)
+    -- utils.highlight("LspReferenceWrite", {
+    --     bg = colors.selection,
+    --     bold = true
+    -- }, config)
 
-	utils.highlight("@variable", { fg = colors.fg }, config)
-	utils.highlight("@variable.builtin", { fg = colors.keyword }, config)
-	utils.highlight("@variable.parameter", { fg = colors.fg }, config)
-	utils.highlight("@variable.member", { fg = colors.property }, config)
+    -- -- LSP signature help
+    -- utils.highlight("LspSignatureActiveParameter", {
+    --     fg = colors.accent1,
+    --     bold = true
+    -- }, config)
 
-	utils.highlight("@type", { fg = colors.type }, config)
-	utils.highlight("@type.builtin", { fg = colors.type }, config)
+    -- -- LSP code lens
+    -- utils.highlight("LspCodeLens", {
+    --     fg = colors.comment,
+    --     italic = true
+    -- }, config)
+    -- utils.highlight("LspCodeLensSeparator", {
+    --     fg = colors.comment
+    -- }, config)
 
-	utils.highlight("@constant", { fg = colors.number }, config)
-	utils.highlight("@constant.builtin", { fg = colors.keyword }, config)
-
-	utils.highlight("@string", { fg = colors.string }, config)
-	utils.highlight("@string.template", { fg = colors.string }, config)
-	utils.highlight("@string.regexp", { fg = colors.accent2 }, config)
-
-	utils.highlight("@number", { fg = colors.number }, config)
-	utils.highlight("@boolean", { fg = colors.keyword }, config)
-
-	utils.highlight("@operator", { fg = colors.operator }, config)
-	utils.highlight("@punctuation.bracket", { fg = colors.operator }, config)
-	utils.highlight("@punctuation.delimiter", { fg = colors.operator }, config)
-	utils.highlight("@punctuation.special", { fg = colors.accent2 }, config)
-
-	utils.highlight("@comment", { fg = colors.comment, italic = true }, config)
-
-	utils.highlight("@tag", { fg = colors.declaration }, config)
-	utils.highlight("@tag.builtin", { fg = colors.declaration }, config)
-	utils.highlight("@tag.attribute", { fg = colors.property }, config)
-	utils.highlight("@tag.delimiter", { fg = colors.operator }, config)
-
-	utils.highlight("@lsp.type.function", { fg = colors.func }, config)
-	utils.highlight("@lsp.type.method", { fg = colors.method }, config)
-	utils.highlight("@lsp.type.member", { fg = colors.method }, config)
-	utils.highlight("@lsp.type.property", { fg = colors.property }, config)
-	utils.highlight("@lsp.type.variable", { fg = colors.fg }, config)
-	utils.highlight("@lsp.type.parameter", { fg = colors.fg }, config)
-	utils.highlight("@lsp.type.class", { fg = colors.type }, config)
-	utils.highlight("@lsp.type.interface", { fg = colors.type }, config)
-	utils.highlight("@lsp.type.enum", { fg = colors.type }, config)
-	utils.highlight("@lsp.type.enumMember", { fg = colors.property }, config)
-	utils.highlight("@lsp.type.namespace", { fg = colors.type }, config)
-	utils.highlight("@lsp.type.decorator", { fg = colors.accent2 }, config)
-	utils.highlight("@lsp.type.typeParameter", { fg = colors.type }, config)
-	utils.highlight("@lsp.type.keyword", { fg = colors.keyword }, config)
-	utils.highlight("@lsp.type.string", { fg = colors.string }, config)
-	utils.highlight("@lsp.type.number", { fg = colors.number }, config)
-	utils.highlight("@lsp.type.operator", { fg = colors.operator }, config)
-	utils.highlight("@lsp.type.comment", { fg = colors.comment }, config)
-
-	utils.highlight("@lsp.mod.readonly", { bold = true }, config)
-	utils.highlight("@lsp.mod.static", { bold = true }, config)
-	utils.highlight("@lsp.mod.deprecated", { strikethrough = true }, config)
+    -- -- LSP inlay hints
+    -- utils.highlight("LspInlayHint", {
+    --     fg = colors.comment,
+    --     bg = colors.line
+    -- }, config)
 end
 
 return M
--- lua/slime/languages/javascript.lua
+++ lua/slime/languages/javascript.lua
@@ -3,62 +3,61 @@ local utils = require("slime.utils")
 local M = {}
 
 function M.setup(colors, config)
-    -- -- JavaScript/TypeScript specific highlights
-    -- utils.highlight("@keyword.function.javascript", {
-    --     fg = colors.declaration
-    -- }, config)
-    -- utils.highlight("@keyword.function.typescript", {
-    --     fg = colors.declaration
-    -- }, config)
-    -- utils.highlight("@function.javascript", {
-    --     fg = colors.func
-    -- }, config)
-    -- utils.highlight("@function.typescript", {
-    --     fg = colors.func
-    -- }, config)
-    -- utils.highlight("@method.javascript", {
-    --     fg = colors.method
-    -- }, config)
-    -- utils.highlight("@method.typescript", {
-    --     fg = colors.method
-    -- }, config)
-    -- utils.highlight("@property.javascript", {
-    --     fg = colors.property
-    -- }, config)
-    -- utils.highlight("@property.typescript", {
-    --     fg = colors.property
-    -- }, config)
-    -- utils.highlight("@type.typescript", {
-    --     fg = colors.type
-    -- }, config)
+    utils.highlight("@keyword.function.javascript", {
+        fg = colors.declaration
+    }, config)
+    utils.highlight("@keyword.function.typescript", {
+        fg = colors.declaration
+    }, config)
+    utils.highlight("@function.javascript", {
+        fg = colors.func
+    }, config)
+    utils.highlight("@function.typescript", {
+        fg = colors.func
+    }, config)
+    utils.highlight("@method.javascript", {
+        fg = colors.method
+    }, config)
+    utils.highlight("@method.typescript", {
+        fg = colors.method
+    }, config)
+    utils.highlight("@property.javascript", {
+        fg = colors.property
+    }, config)
+    utils.highlight("@property.typescript", {
+        fg = colors.property
+    }, config)
+    utils.highlight("@type.typescript", {
+        fg = colors.type
+    }, config)
 
-    -- -- JSX/TSX
-    -- utils.highlight("@tag.jsx", {
-    --     fg = colors.declaration
-    -- }, config)
-    -- utils.highlight("@tag.tsx", {
-    --     fg = colors.declaration
-    -- }, config)
-    -- utils.highlight("@tag.attribute.jsx", {
-    --     fg = colors.property
-    -- }, config)
-    -- utils.highlight("@tag.attribute.tsx", {
-    --     fg = colors.property
-    -- }, config)
+    -- JSX/TSX
+    utils.highlight("@tag.jsx", {
+        fg = colors.declaration
+    }, config)
+    utils.highlight("@tag.tsx", {
+        fg = colors.declaration
+    }, config)
+    utils.highlight("@tag.attribute.jsx", {
+        fg = colors.property
+    }, config)
+    utils.highlight("@tag.attribute.tsx", {
+        fg = colors.property
+    }, config)
 
-    -- -- Template literals
-    -- utils.highlight("@string.template.javascript", {
-    --     fg = colors.string
-    -- }, config)
-    -- utils.highlight("@string.template.typescript", {
-    --     fg = colors.string
-    -- }, config)
-    -- utils.highlight("@punctuation.special.javascript", {
-    --     fg = colors.accent2
-    -- }, config)
-    -- utils.highlight("@punctuation.special.typescript", {
-    --     fg = colors.accent2
-    -- }, config)
+    -- Template literals
+    utils.highlight("@string.template.javascript", {
+        fg = colors.string
+    }, config)
+    utils.highlight("@string.template.typescript", {
+        fg = colors.string
+    }, config)
+    utils.highlight("@punctuation.special.javascript", {
+        fg = colors.accent2
+    }, config)
+    utils.highlight("@punctuation.special.typescript", {
+        fg = colors.accent2
+    }, config)
 end
 
 return M
